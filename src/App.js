460fedb8963f50d4f77c1c7bbb6c367bb1dfa261
Abhishekkp42 <96106223+Abhishekkp42@users.noreply.github.com>
2022-03-10 11:39:02 +0530

    created components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { Groceryinput } from './components/Groceryinput';
 
 function App() {
+
+  // const getData= (data) =>{
+  //   console.log("Data", data)
+  // }
+
   return (
     <div className="App">
-      
+      <Groceryinput />
+
+
+      {/* <Left getData={getData} />
+      <Right /> */}
     </div>
   );
 }
+
+// function Left({getData}) {
+//   const counter= 10;
+//   getData(counter)
+//   return (
+//     <div>
+//       Left: 
+//     </div>
+//   )
+// }
+
 
 export default App;
---
+++ src/components/Groceryinput.jsx
@@ -0,0 +1,8 @@
+export const Groceryinput= () =>{
+	return (
+		<div>
+			<input type="text" />
+			<button>Add</button>
+		</div>
+	)
+}
